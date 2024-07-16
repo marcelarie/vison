cb06c96ab2f08841476fdbfe489e52100845a661
marcelarie <googlillo@gmail.com>
2024-07-16 17:09:01 +0200

    install and test serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // Read a json file in the root folder
+    let json_str = std::fs::read_to_string("data.json").unwrap();
+    // Get the contents of the json file
+    let contents: serde_json::Value = serde_json::from_str(&json_str).unwrap();
+
+    if contents["users"].is_null() {
+        println!("name is null");
+    } else {
+        println!("{}", contents["users"][0]["name"]);
+    }
 }
